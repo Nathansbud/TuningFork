42f175b295d2e3e17ed6078401644bd49d2c00ba
Zachary Amiton <zamiton@outlook.com>
2019-07-25 02:30:13 -0700

    made token stuff its own function, started on sheets stuff...because i can
    - should move this out of tuneparser but dang it it’s where i find
    myself so sue me

--- googleapi.py
+++ googleapi.py
@@ -5,29 +5,44 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/documents.readonly']
 
-def get_document(doc): #Wholesale stolen from Google examples :)
+def make_token(scope, cred_name):
     creds = None
 
-    if os.path.exists('credentials' + os.sep + 'token.pickle'):
-        with open('credentials' + os.sep + 'token.pickle', 'rb') as token:
+    token_path = "credentials" + os.sep + cred_name + "_token.pickle"
+    cred_path = "credentials" + os.sep + cred_name + ".json"
+
+    if os.path.exists(token_path):
+        with open(token_path, 'rb') as token:
             creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
+        # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials' + os.sep + 'credentials.json', SCOPES)
+                cred_path, scope)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('credentials' + os.sep + 'token.pickle', 'wb') as token:
+        with open(token_path, 'wb') as token:
             pickle.dump(creds, token)
 
-    service = build('docs', 'v1', credentials=creds)
+    return creds
 
+
+def get_document(doc): #Wholesale stolen from Google examples :)
+    SCOPES = ['https://www.googleapis.com/auth/documents.readonly']
+    docs_token = make_token(scope=SCOPES, cred_name="docs")
+    service = build('docs', 'v1', credentials=docs_token)
     return service.documents().get(documentId=doc).execute()
+
+
+def get_sheet(sheet, range):
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+    sheets_token = make_token(scope=SCOPES, cred_name="sheets")
+    service = build('sheets', 'v4', credentials=sheets_token)
+    return service.spreadsheets().values().get(spreadsheetId=sheet, range=range).execute()
+
 
 def make_snippet_list_from_doc(doc):
     lyric_doc = get_document(doc)
@@ -48,5 +63,6 @@ def make_snippet_list_from_doc(doc):
     return snippets
 
 if __name__ == '__main__':
-    print(make_snippet_list_from_doc("16WNStYc5qNLGFOujF8EBywvFtIQWq56hhYwrh9PLp8c"))
+    # print(make_snippet_list_from_doc("16WNStYc5qNLGFOujF8EBywvFtIQWq56hhYwrh9PLp8c"))
+    print(get_sheet("1rmQ0ps140rlztd2CtDCaTaWdel8RO6YxHWB03DmIoU0", "Levels!A1:L16").get('values'))
     pass
