d2276251ac5d109178f386be356294dc9a809800
Zachary Amiton <zamiton@outlook.com>
2019-11-09 22:00:19 +0530

    nil check on comment

--- scripts/TuneSwiftly.swift
+++ scripts/TuneSwiftly.swift
@@ -2,10 +2,9 @@ import Foundation
 import iTunesLibrary
 
 let lib = try ITLibrary.init(apiVersion: "1.0")
-
 for item in lib.allMediaItems {
     if item.mediaKind == .kindSong {
-        if item.comments!.contains("Vocal") && !item.comments!.contains("Imbecile") {
+        if item.comments != nil && item.comments!.contains("Vocal") && !item.comments!.contains("Imbecile") {
             print(item.title)
             print(item.artist!.name!)
             print(item.location!)
