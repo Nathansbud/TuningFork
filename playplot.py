a973ae2df4e9099b5d88f56b74cdeac3638f79b4
Zachary Amiton <zamiton@outlook.com>
2020-01-20 00:14:10 +0530

    song comparison play plotter

--- playplot.py
+++ playplot.py
@@ -36,26 +36,42 @@ plot_track("What?", "A Tribe Called Quest")
 
 
 def plot_tracks(*name_artist_pair):
+    name_artist_pair = name_artist_pair[::-1]
     fig, ax = plt.subplots()
     plt.title("Song Comparisons")
     plt.xticks(rotation=45)
     plt.xlabel("Time")
     plots = []
-    colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+    colors = ['red', 'orange', '#FADA5e', 'green', 'blue', 'indigo', 'violet'][::-1]
 
-    
+    #Single Plot
+    # for idx, pair in enumerate(name_artist_pair):
+    #     tracks = track_events(pair[0], pair[1])
+    #     dates_play = [std(d['play_date_utc']) for d in tracks if "play_count" in d]
+    #     dates_skip = [std(d['skip_date']) for d in tracks if "skip_count" in d]
+    #     plots.append(ax.plot_date(dates_skip, ["Skip"]*len(dates_skip), color=colors[idx]))
+    #     plots.append(ax.plot_date(dates_play, ["Play"]*len(dates_play), color=colors[idx]))
+
+    #Stacked Plot
     for idx, pair in enumerate(name_artist_pair):
         tracks = track_events(pair[0], pair[1])
-        dates_play = [std(d['play_date_utc']) for d in tracks if "play_count" in d]
-        dates_skip = [std(d['skip_date']) for d in tracks if "skip_count" in d]
-        plots.append(ax.plot_date(dates_skip, ["Skip"]*len(dates_skip), color=colors[idx]))
-        plots.append(ax.plot_date(dates_play, [f"Play"]*len(dates_play), color=colors[idx]))
+        plays = [std(d['play_date_utc']) for d in tracks if "play_count" in d]
+        plots.append(ax.plot_date(plays, [pair[0]]*len(plays), color=colors[idx]))
 
     ax.set_xlim(right=datetime.today())
-    plt.legend((p[0] for p in plots[::2]), (f"{n[0]} by {n[1]}" for n in name_artist_pair))
+    # plt.legend((p[0] for p in plots), (f"{n[0]} by {n[1]}" for n in name_artist_pair), loc="upper left", bbox_to_anchor=(1,1), prop={'size': 6})
     plt.gcf().autofmt_xdate()
     plt.show()
+    fig.savefig(os.path.join(os.path.dirname(__file__), "graphs", "song_comparisons.png"))
 
 
-plot_tracks(("Cooks", "Still Woozy"), ("What?", "A Tribe Called Quest"), ("Flamingo", "Kero Kero Bonito"), ("Yonkers", "Tyler, the Creator"))
+plot_tracks(
+    ("Cooks", "Still Woozy"),
+    ("What?", "A Tribe Called Quest"),
+    ("Mr. Clean", "Yung Gravy"),
+    ("Yonkers", "Tyler, the Creator"),
+    ("Won't Trade", "Q-Tip"),
+    ("Truth Hurts", "Lizzo"),
+    ("Knight Fork", "Feed Me Jack")
+)
 
