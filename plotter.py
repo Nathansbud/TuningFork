ea300dac88005b35c0003d4aa49a9cd58431288a
Zachary Amiton <zamiton@outlook.com>
2020-01-21 14:35:14 +0530

    time plot on hours of day

--- plotter.py
+++ plotter.py
@@ -4,9 +4,11 @@ import matplotlib.pyplot as plt
 from datetime import datetime, date
 from pandas.plotting import register_matplotlib_converters
 
-register_matplotlib_converters()
 with open(os.path.join(os.path.dirname(__file__), "data", "log.txt"), "r") as lf: lines = [json.loads(l) for l in lf.readlines()]
 lines.sort(key=lambda l: l['skip_date'] if 'skip_date' in l else l['play_date_utc'])
+plot_folder = os.path.join(os.path.dirname(__file__), "plots")
+
+register_matplotlib_converters()
 
 def std(date_string): return datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%SZ")
 def track_events(name, artist): return [e for e in lines if e['name'] == name and e['artist'] == artist]
@@ -30,7 +32,7 @@ def plot_track(name, artist):
     plt.gcf().autofmt_xdate()
     plt.show()
 
-def plot_tracks(*name_artist_pair):
+def play_plot(*name_artist_pair):
     name_artist_pair = name_artist_pair[::-1]
     fig, ax = plt.subplots()
     plt.title("Song Comparisons")
@@ -47,10 +49,30 @@ def plot_tracks(*name_artist_pair):
     ax.set_xlim(right=datetime.today())
     plt.gcf().autofmt_xdate()
     plt.show()
-    fig.savefig(os.path.join(os.path.dirname(__file__), "graphs", "song_comparisons.png"))
+    fig.savefig(os.path.join(plot_folder, "song_comparisons.png"))
+
+def time_plot():
+    hours = {i:0 for i in range(0, 24)}
+    for t in [std(d['play_date_utc']) for d in lines if "play_count" in d]:
+        #offset UTC+5:30 (since i'm typically in mumbai)
+        if 29 < t.minute < 60: hours[(t.hour+6)%24]+=1
+        else: hours[(t.hour+5)%24]+=1
+
+    #blue color gradient, #00n0n0, n from 0 to C then down to 1 (behavior of -|x-12|+12)
+    colors = [f"#00{hex(-abs(i-12)+12)[-1]}0{hex(-abs(i - 12)+12)[-1]}0" for i in range(24)]
+    fig, ax = plt.subplots()
+    plt.bar(hours.keys(), hours.values(), color=colors)
+    plt.xticks(list(hours.keys()))
+    ax.set_xlim([-1, 24])
+    plt.xlabel("Hours (Military)")
+    plt.ylabel("Plays")
+    plt.title("Plays Per Hour")
+    plt.show()
+    fig.savefig(os.path.join(plot_folder, "play_times.png"))
 
 
-plot_tracks(
+
+play_plot(
     ("Cooks", "Still Woozy"),
     ("What?", "A Tribe Called Quest"),
     ("Mr. Clean", "Yung Gravy"),
@@ -59,4 +81,7 @@ plot_tracks(
     ("Truth Hurts", "Lizzo"),
     ("Knight Fork", "Feed Me Jack")
 )
+
+time_plot()
+
 
