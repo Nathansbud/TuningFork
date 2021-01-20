503236e721355a8a5870bfa15f129e2e3f96de60
Zack Amiton <zamiton@outlook.com>
2021-01-19 20:22:08 -0800

    Add cli queuer

--- enqueue.py
+++ enqueue.py
@@ -1,49 +1,40 @@
-#!/Users/zackamiton/Code/TuningFork/venv/bin/python
 from migrator import get_token
 from utilities import call_applescript
+
 import time
+import sys
 
-def enqueue():
-    song_dialog = """    
-    set songToQueue to ""
-    tell application "System Events"
-	    set songToQueue to text returned of (display dialog "Input Track:" default answer "" with title "Add to Queue")
-    end tell
-    songToQueue
-    """
 
-    #Not Working
-    # open_spotify = """
-    # if application "Spotify" is not running
-    #     tell application "Spotify"
-    #         play
-    #         set priorVolume to sound volume as text
-    #         set pv to priorVolume
-    #         set sound volume to 0
-    #         "RAN"
-    #     end tell
-    # end if
-    # """
-    #
-    # reset_volume_and_skip = """
-    # tell application "Spotify"
-    #     set sound volume to 100
-    # end tell
-    # """
+def enqueue(song, artist=None):
+    spotify = get_token()
+    if artist:
+        st = spotify.get(f"https://api.spotify.com/v1/search/?q={song}%20artist:{artist}&type=track&limit=1&offset=0").json()
+    else: 
+        st = spotify.get(f"https://api.spotify.com/v1/search/?q={song}&type=track&limit=1&offset=0").json()
 
-    song_input = call_applescript(song_dialog)['output'].strip()
-    if song_input:
-        # conditional_open = call_applescript(open_spotify)['output'].strip()
-        # if conditional_open: time.sleep(2)
-        song, *artist = song_input.lower().split(" by ")
-        spotify = get_token()
-        if song and artist: st = spotify.get(f"https://api.spotify.com/v1/search/?q={song}%20artist:{artist[0]}&type=track&limit=1&offset=0").json()
-        else: st = spotify.get(f"https://api.spotify.com/v1/search/?q={song}&type=track&limit=1&offset=0").json()
+    track_uri = st['tracks']['items'][0]['uri'] if st['tracks']['items'] else ""
+    
+    spotify.post(f"https://api.spotify.com/v1/me/player/queue?uri={track_uri}")
+    print("Added track to queue!")
 
-        track_uri = st['tracks']['items'][0]['uri'] if st['tracks']['items'] else ""
-        print(spotify.post(f"https://api.spotify.com/v1/me/player/queue?uri={track_uri}"))
-        # if conditional_open: call_applescript(reset_volume_and_skip.replace("[VOLUME]", conditional_open))
 
+def queue_track(mode='dialog'):
+    if mode == 'dialog':
+        song_dialog = """    
+        set songToQueue to ""
+        tell application "System Events"
+    	    set songToQueue to text returned of (display dialog "Input Track:" default answer "" with title "Add to Queue")
+        end tell
+        songToQueue
+        """
+
+        song_input = call_applescript(song_dialog)['output'].strip()
+        if song_input:
+            song, *artist = song_input.lower().split(" by ")
+            enqueue(song, artist)
+    
+    elif 'cli':
+        enqueue(*sys.argv[1:])
 
 if __name__ == '__main__':
-    enqueue()
+    queue_track('dialog' if not sys.argv[1:] else 'cli')
