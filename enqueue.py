b6eff22beeb22f23146e68bedb9abbe46b311647
Zack Amiton <zamiton@outlook.com>
2021-01-30 18:57:22 -0800

    Add option to re-queue last n played

--- enqueue.py
+++ enqueue.py
@@ -4,24 +4,36 @@ import time
 import sys
 import argparse
 
-def enqueue(song, artist=None, times=1):
+def enqueue(song, artist=None, times=1, last=None):
     spotify = get_token()
-    track_uri =  ''
+    track_uris = []
+    
     if song: 
         if artist:
             st = spotify.get(f"https://api.spotify.com/v1/search/?q={song}%20artist:{artist}&type=track&limit=1&offset=0").json()
         else: 
             st = spotify.get(f"https://api.spotify.com/v1/search/?q={song}&type=track&limit=1&offset=0").json()
 
-        track_uri = st['tracks']['items'][0]['uri'] if st['tracks']['items'] else ""
-    else:
+        track_uris = [st['tracks']['items'][0]['uri']] if st['tracks']['items'] else []
+    
+    elif not last:
         current = spotify.get("https://api.spotify.com/v1/me/player/currently-playing").json()
-        track_uri = current.get('item', {}).get('uri')
+        track_uris = [current.get('item', {}).get('uri')]
+    else:
+        previous = spotify.get(f"https://api.spotify.com/v1/me/player/recently-played?limit={last}").json()
+        track_uris = [s.get('track', {}).get('uri') for s in previous.get('items')][::-1]
 
-    if track_uri:
+    if track_uris:
         for i in range(times):  
-           spotify.post(f"https://api.spotify.com/v1/me/player/queue?uri={track_uri}")
-        print(f"Added {'currently playing track' if not song else song.strip()} to queue ({times}T)!")
+           for uri in track_uris:
+               spotify.post(f"https://api.spotify.com/v1/me/player/queue?uri={uri}")
+        
+        if last:
+            print(f"Added{' ' + str(last) if last > 1 else ''} last played item{'s' if last > 1 else ''} to queue {times}x!")
+        elif not song:
+            print(f"Added currently playing track to queue {times}x!")
+        else:
+            print(f"Added {song.strip()} to queue {times}x!")
 
 def queue_track(mode='dialog'):
     if mode == 'dialog':
@@ -44,9 +56,12 @@ def queue_track(mode='dialog'):
         parser.add_argument('title', nargs='?', default=None)
         parser.add_argument('artist', nargs='?', default=None)
         parser.add_argument('-t', '--times', default=1, type=int)
+        parser.add_argument('-p', '--previous', default=0, type=int)
 
         args = parser.parse_args()
-        enqueue(args.title, args.artist, args.times)
+        enqueue(args.title, args.artist, args.times, args.previous)
+
+
 
 if __name__ == '__main__':
     queue_track('cli')
