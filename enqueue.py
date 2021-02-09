3d2e7f8180f83a6fd9e2541d100be8df2d33c503
Zack Amiton <zamiton@outlook.com>
2021-02-08 19:38:28 -0800

    Fix case of non-playing current track queue (+ make its output a bit more detailed)

--- enqueue.py
+++ enqueue.py
@@ -99,20 +99,24 @@ def enqueue(title=None, artist=None, times=1, last=None, group=None):
         group_data = groups.get(group, [])
         track_uris = [t.get('uri') for t in group_data] 
         
-    if title: 
+    elif title: 
         if artist:
             st = spotify.get(f"https://api.spotify.com/v1/search/?q={title}%20artist:{artist}&type=track&limit=1&offset=0").json()
         else: 
             st = spotify.get(f"https://api.spotify.com/v1/search/?q={title}&type=track&limit=1&offset=0").json()
 
         track_uris = [st['tracks']['items'][0]['uri']] if st['tracks']['items'] else []
-    
-    elif not last:
-        current = spotify.get("https://api.spotify.com/v1/me/player/currently-playing").json()
-        track_uris = [current.get('item', {}).get('uri')]
-    else:
+    elif last:
         previous = spotify.get(f"https://api.spotify.com/v1/me/player/recently-played?limit={last}").json()
         track_uris = [s.get('track', {}).get('uri') for s in previous.get('items')][::-1]
+    else:
+        current = spotify.get("https://api.spotify.com/v1/me/player/currently-playing")
+        if current.status_code == 204: 
+            print("No track currently playing!")
+        else:
+            data = current.json().get('item', {})
+            track_uris = [data.get('uri')]
+            group_data = [{'name': data.get('name'), 'artist': ', '.join([artist.get('name') for artist in data.get('artists', [])])}]
 
     if track_uris:
         for i in range(times):  
@@ -121,10 +125,10 @@ def enqueue(title=None, artist=None, times=1, last=None, group=None):
 
         if last:
             print(f"Added{' ' + str(last) if last > 1 else ''} last played item{'s' if last > 1 else ''} to queue {times}x!")
-        elif group:
+        elif group or not title:
             print("Added " + ", ".join([f"{t.get('name')} by {t.get('artist')}" for t in group_data]) + f" to queue {times}x!")
-        elif not title:
-            print(f"Added currently playing track to queue {times}x!")
+        # elif not title:
+        #     print(f"Added currently playing track to queue {times}x!")
         else:
             print(f"Added {title.strip()} to queue {times}x!")
 
