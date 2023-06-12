25db606e5ccae6cec34fef7f460b3466be165871
Zack Amiton <zachary.amiton@wnco.com>
2023-06-12 12:40:37 -0500

    remove next from skip (ambiguous that it skips current

--- enqueue.py
+++ enqueue.py
@@ -390,7 +390,7 @@ def queue_track():
         
         exit(0)
     elif args.next and args.next > 0:
-        print(f"Attempting to skip next {color(args.next, Colors.WHITE)} {color('tracks', Colors.WHITE)}...")
+        print(f"Attempting to skip {color(args.next, Colors.WHITE)} {color('tracks', Colors.WHITE)}...")
         # This is probably not the optimal way to do this...but if we get rate limited, so be it
         for _ in range(args.next): 
             _ = spotify.post("https://api.spotify.com/v1/me/player/next")
